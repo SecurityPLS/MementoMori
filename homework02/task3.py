63035cccd583156684099d33b239101947cb187f
SecurityPLS <125352280+SecurityPLS@users.noreply.github.com>
2023-05-08 07:26:13 +0300

    Delete homework02 directory

--- homework02/task3.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Any,Tuple
-from itertools import product
-
-
-def combinations(*args: List[Any]) -> List[Tuple]:
-    all_combinations = product(*args)
-    return list(all_combinations)
