1224bf32a8cf21080be6987c634a95fa70ac4fe2
NOVOS <novoselbcev00@mail.ru>
2023-05-08 14:17:20 +0300

    add file

--- homework02/task2.py
+++ homework02/task2.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
-    counter = Counter(inp)  #
+    counter = Counter(inp)
     rare_letter = min(counter, key=counter.get)
     most_common_letter = max(counter, key=counter.get)
     return most_common_letter, rare_letter
